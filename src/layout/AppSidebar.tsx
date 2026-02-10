6a844ae1a11a32c09c797cddc9ccf08c91df8f31
Jean Arielle Santos <jean_arielle_santos@dlsu.edu.ph>
2026-02-11 02:26:35 +0800

    cleanr and fix logo and image loading

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -365,14 +365,14 @@ const filteredNavItems = navItems
             <>
               <img
                 className="dark:hidden"
-                src="/images/logo/logo.svg"
+                 src={`${import.meta.env.BASE_URL}images/logo/logo.svg`}
                 alt="Logo"
                 width={150}
                 height={40}
               />
               <img
                 className="hidden dark:block"
-                src="/images/logo/logo-dark.svg"
+                 src={`${import.meta.env.BASE_URL}images/logo/logo-dark.svg`}
                 alt="Logo"
                 width={150}
                 height={40}
--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,9 +1,9 @@
-import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
-import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
-import StatisticsChart from "../../components/ecommerce/StatisticsChart";
-import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
-import RecentOrders from "../../components/ecommerce/RecentRequests";
-import DemographicCard from "../../components/ecommerce/DemographicCard";
+// import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
+// import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
+// import StatisticsChart from "../../components/ecommerce/StatisticsChart";
+// import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
+// import RecentOrders from "../../components/ecommerce/RecentRequests";
+// import DemographicCard from "../../components/ecommerce/DemographicCard";
 import PageMeta from "../../components/common/PageMeta";
 import RecentRequests from "../../components/ecommerce/RecentRequests";
 
@@ -38,11 +38,11 @@ export default function Home() {
         </div>
       </div> */}
 
-            <div className="grid grid-cols-12 gap-4 md:gap-6">
-              <div className="col-span-12  space-y-6 xl:col-span-12">
-                <RecentRequests />
-              </div>
-            </div>
+        <div className="grid grid-cols-12 gap-4 md:gap-6">
+          <div className="col-span-12  space-y-6 xl:col-span-12">
+            <RecentRequests />
+          </div>
+        </div>
     </>
   );
 }
